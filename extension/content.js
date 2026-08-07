c5181e0c6733d73ee2fd46ca46a4d2241e29212a
FredB30 <fbarnoin@wanadoo.fr>
2026-08-07 13:25:34 +0200

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,16 @@
+(() => {
+  "use strict";
 
+  console.log("Feeligrane loaded.");
+
+  const article =
+    document.querySelector("article") ||
+    document.querySelector('[data-testid="post-content"]');
+
+  if (!article) {
+    console.log("Feeligrane: no article content found.");
+    return;
+  }
+
+  console.log("Feeligrane: article detected.", article);
+})();
